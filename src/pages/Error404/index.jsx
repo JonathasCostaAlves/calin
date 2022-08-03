1770fbb2cdd3ed7a96795994aec3d54f3868fa08
Jonathas Costa <jcalves182@gmail.com>
2022-08-03 13:50:12 -0300

    Alteração de componente Footer para recarregar a pagina quando clicada

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -9,7 +9,7 @@ import relax from '../../assets/imgs/rilex-img.png'
 export default props =>
     <>
         <Header bg={true} />
-        <main className="pageError d-flex flex-column justify-content-center align-items-center">
+        <main className="pageError d-flex flex-column justify-content-center align-items-center text-center">
             <h1>Calin</h1>
             <img src={relax} alt="" />
             <h2 className="mb-5"> Pagina em construção em breve novidades para você! <Link to='/'>ir para inicio</Link></h2>
